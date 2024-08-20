96f5070c7127659e6c301f7eff6400f35eca6f1d
Johnny Garcia <johnnygar98@hotmail.com>
2024-08-19 22:40:12 -0500

    adding the api backend folder, we are going to use java' '

---
+++ api-serena/src/main/java/com/taguz91/api_serena/ApiSerenaApplication.java
@@ -0,0 +1,13 @@
+package com.taguz91.api_serena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSerenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSerenaApplication.class, args);
+	}
+
+}
---
+++ api-serena/src/test/java/com/taguz91/api_serena/ApiSerenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taguz91.api_serena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSerenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
