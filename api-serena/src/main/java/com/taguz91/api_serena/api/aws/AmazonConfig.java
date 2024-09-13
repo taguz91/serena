2d1677cc77eef1978f1ac8317f3681f9eeec6e43
Johnny Garcia <johnnygar98@hotmail.com>
2024-09-12 19:07:08 -0500

    using s3 with credentials and region config

--- api-serena/src/main/java/com/taguz91/api_serena/api/aws/AmazonConfig.java
+++ api-serena/src/main/java/com/taguz91/api_serena/api/aws/AmazonConfig.java
@@ -1,18 +1,34 @@
 package com.taguz91.api_serena.api.aws;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Value("${aws.s3.region}")
+    private String bucketRegion;
+
+    @Value("${aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
     @Bean
     public AmazonS3 s3() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
         return AmazonS3ClientBuilder
                 .standard()
-                .withRegion("us-east-2")
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(awsCredentials)
+                ).withRegion(bucketRegion)
                 .build();
     }
 }
