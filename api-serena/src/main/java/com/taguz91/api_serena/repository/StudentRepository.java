09806fd9e84674567d22cf7b36688201206f518e
Johnny Garcia <johnnygar98@hotmail.com>
2025-01-28 22:34:43 -0500

    Avoid duplicate registers

--- api-serena/src/main/java/com/taguz91/api_serena/repository/StudentRepository.java
+++ api-serena/src/main/java/com/taguz91/api_serena/repository/StudentRepository.java
@@ -28,7 +28,7 @@ public interface StudentRepository extends JpaRepository<Student, String> {
     Optional<Student> findByIdentification(@Param("identification") String identification);
 
     @Query(
-            value = "SELECT s.id, s.identification, s.name, s.lastname, s.gender, s.reference, s.created_at, s.updated_at, s.created_by, s.updated_by, s.is_deleted "
+            value = "SELECT distinct s.id, s.identification, s.name, s.lastname, s.gender, s.reference, s.created_at, s.updated_at, s.created_by, s.updated_by, s.is_deleted "
                     + QUERY_BY_TEACHER
                     +  " ORDER BY s.name ASC LIMIT :limitParam OFFSET :offset "
                     + "\n-- #pageable\n",
