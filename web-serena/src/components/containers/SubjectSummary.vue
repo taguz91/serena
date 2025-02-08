510e0092228681bcaeb3581d53f59403f1250274
Johnny Garcia <johnnygar98@hotmail.com>
2025-02-08 14:00:33 -0500

    new charts and icon

--- web-serena/src/components/containers/SubjectSummary.vue
+++ web-serena/src/components/containers/SubjectSummary.vue
@@ -3,6 +3,7 @@
     <div class="flex items-center justify-between">
       <div class="flex justify-between w-full mr-6">
         <p class="text-lg">{{ studentSubject.name }}</p>
+        <p class="text-slate-400 mt-2">Docente: {{ studentSubject.teacher }}</p>
       </div>
 
       <div class="self-center cursor-pointer" @click="loadChart">
@@ -12,38 +13,9 @@
       </div>
     </div>
 
-    <div :hidden="hidden" :class="hidden ? '' : 'flex justify-between p-5 mx-14'">
+    <div :hidden="hidden" :class="hidden ? '' : 'p-5 mx-14'">
       <div class="w-full">
-        <Chart
-          direction="circular"
-          :size="{ width: 300, height: 300 }"
-          :config="{ controlHover: false }"
-          :data="summary"
-        >
-          <template #layers>
-            <Pie
-              :data-keys="['emotion', 'count']"
-              :pie-style="{ innerRadius: 100, padAngle: 0.01 }"
-            />
-          </template>
-
-          <template #widgets>
-            <Tooltip
-              :config="{
-                emotion: { label: 'Estado' },
-                count: { label: 'Total' }
-              }"
-            />
-          </template>
-        </Chart>
-      </div>
-
-      <div class="mr-10 w-40">
-        <h2 class="text-xl font-bold mb-2">Estados</h2>
-        <p v-for="emotion in summary" :key="emotion.emotion">
-          <span class="font-bold text-xl">{{ emotion.count }}</span>
-          {{ emotionLabel(emotion.emotion) }}
-        </p>
+        <Pie v-if="dataset" :data="dataset" :options="pieOptions" />
       </div>
     </div>
   </div>
@@ -53,12 +25,14 @@
 import { ref } from 'vue'
 import { NIcon } from 'naive-ui'
 
-import { Chart, Pie, Tooltip } from 'vue3-charts'
+import { Pie } from 'vue-chartjs'
 
 import { fetchWrapper } from '@/helpers/fetch_wrapper'
 import type { StudentSubject, Summary } from '@/interfaces'
 import { CaretDown } from '@vicons/tabler'
-import { emotionLabel } from '@/utils/translate'
+import { emotionColor, emotionLabel } from '@/utils/translate'
+import type { ChartData } from 'chart.js'
+import { loadChartConfig, pieOptions } from '@/helpers/loadChartConfig'
 
 const props = defineProps<{
   idStudent: string
@@ -68,8 +42,11 @@ const props = defineProps<{
 
 const hidden = ref(true)
 const summary = ref<Summary[]>([])
+const dataset = ref<ChartData<'pie'> | undefined>()
 
 const loadChart = async () => {
+  await loadChartConfig()
+
   if (!hidden.value) {
     hidden.value = true
     return
@@ -80,6 +57,17 @@ const loadChart = async () => {
     `/v1/student/summary/${props.idStudent}/subject/${props.studentSubject.id}?periods=${props.idAcademicPeriods.join(',')}`
   )
 
-  summary.value = data
+  summary.value = data.sort((a, b) => b.count - a.count)
+
+  dataset.value = {
+    labels: summary.value.map((s) => `${s.count} - ${emotionLabel(s.emotion)}`),
+    datasets: [
+      {
+        label: 'Estudiantes',
+        backgroundColor: summary.value.map((s) => emotionColor(s.emotion)),
+        data: summary.value.map((s) => s.count)
+      }
+    ]
+  }
 }
 </script>
--- web-serena/src/utils/translate.ts
+++ web-serena/src/utils/translate.ts
@@ -10,8 +10,24 @@ const EMOTIONS: Record<string, string> = {
   FEAR: 'Asustado'
 }
 
+const EMOTIONS_COLORS: Record<string, string> = {
+  HAPPY: '#FFFFCC', // Lighter Yellow
+  SAD: '#CCCCFF', // Lighter Blue
+  ANGRY: '#FFCCCC', // Lighter Red
+  CONFUSED: '#E6CCE6', // Lighter Purple
+  DISGUSTED: '#CCFFCC', // Lighter Green
+  SURPRISED: '#FFE6CC', // Lighter Orange
+  CALM: '#CCFFFF', // Lighter Cyan
+  UNKNOWN: '#E0E0E0', // Lighter Gray
+  FEAR: '#D9B3B3' // Lighter Maroon
+}
+
 export const emotionLabel = (emotion: string) => {
   return EMOTIONS[emotion] ?? emotion
+}
+
+export const emotionColor = (emotion: string) => {
+  return EMOTIONS_COLORS[emotion] ?? '#000000'
 }
 
 const STATUS: Record<string, string> = {
